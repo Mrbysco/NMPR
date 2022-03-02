49a65cf568bdafdf0a1c52893ed508ddae7f4b83
Mrbysco <Mrbysco@users.noreply.github.com>
2022-03-02 23:20:52 +0100

    Update to 1.18.2

--- src/main/java/com/NoPoisonRegen/Main/Main.java
+++ src/main/java/com/NoPoisonRegen/Main/Main.java
@@ -10,31 +10,31 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod(Main.MOD_ID)
 public class Main {
-   public static final String MOD_ID = "nopoisonregen";
+	public static final String MOD_ID = "nopoisonregen";
 
-   public Main() {
-      MinecraftForge.EVENT_BUS.register(this);
-   }
+	public Main() {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 
-   @SubscribeEvent
-   public void playerTickEvent(PlayerTickEvent event) {
-      Player player = event.player;
-      if (player.hasEffect(MobEffects.REGENERATION) && player.hasEffect(MobEffects.POISON)) {
-         int regenTime = player.getEffect(MobEffects.REGENERATION).getDuration();
-         int poisonTime = player.getEffect(MobEffects.POISON).getDuration();
-         int remainingTime = regenTime - poisonTime;
-         if (remainingTime < 0) {
-            player.removeEffectNoUpdate(MobEffects.POISON);
-            player.removeEffectNoUpdate(MobEffects.REGENERATION);
-            player.addEffect(new MobEffectInstance(MobEffects.POISON, -remainingTime));
-         } else if (remainingTime > 0) {
-            player.removeEffectNoUpdate(MobEffects.POISON);
-            player.removeEffectNoUpdate(MobEffects.REGENERATION);
-            player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, remainingTime));
-         } else {
-            player.removeEffectNoUpdate(MobEffects.POISON);
-            player.removeEffectNoUpdate(MobEffects.REGENERATION);
-         }
-      }
-   }
+	@SubscribeEvent
+	public void playerTickEvent(PlayerTickEvent event) {
+		Player player = event.player;
+		if (player.hasEffect(MobEffects.REGENERATION) && player.hasEffect(MobEffects.POISON)) {
+			int regenTime = player.getEffect(MobEffects.REGENERATION).getDuration();
+			int poisonTime = player.getEffect(MobEffects.POISON).getDuration();
+			int remainingTime = regenTime - poisonTime;
+			if (remainingTime < 0) {
+				player.removeEffectNoUpdate(MobEffects.POISON);
+				player.removeEffectNoUpdate(MobEffects.REGENERATION);
+				player.addEffect(new MobEffectInstance(MobEffects.POISON, -remainingTime));
+			} else if (remainingTime > 0) {
+				player.removeEffectNoUpdate(MobEffects.POISON);
+				player.removeEffectNoUpdate(MobEffects.REGENERATION);
+				player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, remainingTime));
+			} else {
+				player.removeEffectNoUpdate(MobEffects.POISON);
+				player.removeEffectNoUpdate(MobEffects.REGENERATION);
+			}
+		}
+	}
 }
