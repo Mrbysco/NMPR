8888e0a679f6f1019904e81ebfd44cb6631c575b
Mrbysco <Mrbysco@users.noreply.github.com>
2024-06-14 20:16:49 +0200

    Update to 1.20.6

--- forge/src/main/java/com/nopoisonregen/NMPRNeoForge.java
+++ forge/src/main/java/com/nopoisonregen/NMPRNeoForge.java
@@ -1,14 +1,14 @@
 package com.nopoisonregen;
 
+import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.entity.living.LivingEvent;
 import net.neoforged.neoforge.event.entity.living.MobEffectEvent;
+import net.neoforged.neoforge.event.tick.EntityTickEvent;
 
 @Mod(Constants.MOD_ID)
 public class NMPRNeoForge {
@@ -24,11 +24,12 @@ public class NMPRNeoForge {
 	private void isEffectApplicable(MobEffectEvent.Applicable event) {
 		boolean isApplicable = CommonClass.isEffectApplicable(event.getEffectInstance(), event.getEntity());
 		if (!isApplicable) {
-			event.setResult(Event.Result.DENY);
+			event.setResult(MobEffectEvent.Applicable.Result.DO_NOT_APPLY);
 		}
 	}
 
-	private void onLivingTick(LivingEvent.LivingTickEvent event) {
-		CommonClass.onLivingTick(event.getEntity());
+	private void onLivingTick(EntityTickEvent.Post event) {
+		if (event.getEntity() instanceof LivingEntity livingEvent)
+			CommonClass.onLivingTick(livingEvent);
 	}
 }
