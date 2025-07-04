e355caeabb6ca52a96201ae43965a2f9a877f082
Mrbysco <Mrbysco@users.noreply.github.com>
2025-07-04 22:32:30 +0200

    Increment version

--- src/main/java/com/nopoisonregen/Main.java
+++ src/main/java/com/nopoisonregen/Main.java
@@ -14,7 +14,7 @@ import org.apache.logging.log4j.Logger;
 public class Main {
 	public static final String MOD_ID = "nopoisonregen";
 	public static final String NAME = "No Poison with Regeneration";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 
